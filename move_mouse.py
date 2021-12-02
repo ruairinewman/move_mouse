a64f4757ef9c834a89b02498138731de6a78c89a
Ruairí Newman <github@rnewman.eu>
2021-12-02 13:06:00 +0000

    aded interval and expiry args

--- move_mouse.py
+++ move_mouse.py
@@ -1,18 +1,39 @@
 #!/usr/bin/env python3
 
+import sys
+import argparse
 import pyautogui
 from random import randrange
 from pynput.mouse import Button, Controller
 from time import sleep
+from time import time
 
 mouse = Controller()
-width, height= pyautogui.size()
+width, height= pyautogui.size() # compatibility issue with Apple Retina screen? Reports 1680x1050 instead of 2880×1800
 interval = 60 # seconds to sleep after each movement of mouse pointer
+
+# Argument parsing
+parser = argparse.ArgumentParser(description="Arguments")
+parser.add_argument("-e", "--expiry", help = "Expiry in minutes ; default is None", required=False, default=None, type=int)
+parser.add_argument("-i", "--interval", help = "Frequency in minutes of mouse pointer moves ; default is 1 (every minute)", required=False, default=1, type=int)
+args = parser.parse_args()
+
+# Set expiry
+if args.expiry is None:
+	expiry = None
+else:
+	expiry = (args.expiry * 60)
+
+# Set interval
+if args.interval is None:
+	interval = None
+else:
+	interval = (args.interval * 60)
 
 # print detected screen resolution - information only
 print("Detected screen resolution:", str(width) + 'x' + str(height))
 
-def move_mouse(w, h, i):
+def move_mouse(w, h, i, e):
 	# get random coordinates for mouse
 	wpos = randrange(0, w)
 	hpos = randrange(0, h)
@@ -23,6 +44,13 @@ def move_mouse(w, h, i):
 	# sleep for a minute
 	sleep(i)
 
-while True:
-	move_mouse(width, height, interval)
-
+time_start = time()
+if expiry is None:
+	while True:
+		move_mouse(width, height, interval, expiry)
+else:
+	try:
+		while time() < time_start + expiry:
+			move_mouse(width, height, interval, expiry)
+	except:
+		sys.exit(1)
